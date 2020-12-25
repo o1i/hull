2a4bd244567ace1e104205210525f6a3cf5b1ae9
oliver <oliver.burkhard@gmail.com>
2020-12-25 19:34:34 +0100

    Add first version of implied vol

--- methods/eur_opt_implied_vol.py
+++ methods/eur_opt_implied_vol.py
@@ -1,1 +1,18 @@
+from typing import Callable
 
+import numpy as np
+from scipy.optimize import minimize, Bounds
+
+
+def implied_vol_wrapper(func: Callable, price: float, **kwargs):
+    """
+    Finds the volatility yielding the searched for price
+    :param func: Function to be used for price finding. "sig" must be a valid argument.
+    :param price: target price
+    :param kwargs: all required arguments for func except sig
+    :return: target sig
+    """
+    def deviation(sig):
+        return (func(sig=sig, **kwargs) - price) ** 2
+
+    return minimize(deviation, [0.2], bounds=Bounds([0], [np.inf])).x[0]
